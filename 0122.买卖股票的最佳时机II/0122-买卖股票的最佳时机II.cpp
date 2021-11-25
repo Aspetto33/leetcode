0b0ff028b69be2a430e945adca7be917742a6409
‘Aspetto33 <1760906445@qq.com>
2021-11-25 10:02:15 +0800

    增加对求“最长递增子序列”题解的实现

--- 0122.买卖股票的最佳时机II/0122-买卖股票的最佳时机II.cpp
+++ 0122.买卖股票的最佳时机II/0122-买卖股票的最佳时机II.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int result = 0; //记录最终结果
-        //从第二个元素开始，用后一个元素减去前一个元素
-        for(int i=1;i<prices.size();i++){
-            result+= max((prices[i]-prices[i-1]),0);    //只加大于零的差值
+        int length = prices.size();
+        vector<vector<int>> dp(length,vector<int>(2,0));
+
+        //初始化,表示第一天持有股票获得的钱数
+        dp[0][0] -=prices[0];
+
+        //表示第一天不持有股票获得的钱数
+        dp[0][1] = 0;
+        for(int i = 1;i<length;i++){
+            dp[i][0] = max(dp[i-1][0],dp[i-1][1]-prices[i]);
+            dp[i][1] = max(dp[i-1][1],dp[i-1][0]+prices[i]);
         }
-        return result;
+        return dp[length-1][1];
     }
 };
