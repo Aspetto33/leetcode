895f533d534a4f83a8afe908710df6b31f4bd375
‘Aspetto33 <1760906445@qq.com>
2021-12-03 09:51:33 +0800

    增加对求“轮转数组”题解的实现

--- 0977.有序数组的平方/0977-有序数组的平方.cpp
+++ 0977.有序数组的平方/0977-有序数组的平方.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-       vector<int> result(nums.size(),0);
-       int k=nums.size()-1;
-       int i=0;
-       int j=nums.size()-1;
-       while(i<=j){
-           if(nums[i]*nums[i]<nums[j]*nums[j]){
-               result[k]=nums[j]*nums[j];
-               k--;
-               j--;
-           }else{
-               result[k]=nums[i]*nums[i];
-               k--;
-               i++;
-           }
-       }
-       return result;
+        vector<int> result(nums.size(),0);
+
+        int leftIndex = 0;
+        int rightIndex = nums.size()-1;
+
+        int resultIndex = result.size()-1;
+
+        while(leftIndex<=rightIndex){
+            if(nums[leftIndex]*nums[leftIndex]<=nums[rightIndex]*nums[rightIndex]){
+                result[resultIndex] = nums[rightIndex]*nums[rightIndex];
+                resultIndex--;
+                rightIndex--;
+            }else{
+                result[resultIndex] = nums[leftIndex]*nums[leftIndex];
+                resultIndex--;
+                leftIndex++;
+            }
+        }
+        return result;
     }
 };
