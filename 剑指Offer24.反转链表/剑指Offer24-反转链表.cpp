d5a30e5dc9364fc1821ffa485fb42998abd17c1b
‘Aspetto33 <1760906445@qq.com>
2021-12-17 23:51:52 +0800

    增加对求“从头到尾打印链表、反转链表”题解的实现

--- 剑指Offer24.反转链表/剑指Offer24-反转链表.cpp
+++ 剑指Offer24.反转链表/剑指Offer24-反转链表.cpp
@@ -3,25 +3,22 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* cur = head;   //快指针指向head
-        ListNode* pre = nullptr;    //慢指针，指向head前面一个空节点
+        ListNode* tmp = nullptr;
 
-        //cur不为空的情况下进行循环，改变指针方向
+        ListNode* cur = head;
+
         while(cur){
-            ListNode* tmp = cur->next;  //保存cur下一个节点的指针
-            cur->next = pre;    //cur指针改变方向
-            pre = cur;  //pre移动到cur处
-            cur = tmp;  //cur移动到下一个节点处
+            ListNode* t = cur->next;
+            cur->next = tmp;
+            tmp = cur;
+            cur = t;
         }
-        return pre;
-
+        return tmp;
     }
 };
