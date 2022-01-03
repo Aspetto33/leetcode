9c44925fc16d59ed7fe2cf1c4219c8a2131e93f5
‘Aspetto33 <1760906445@qq.com>
2022-01-03 09:30:09 +0800

    搜索与回溯

--- 剑指Offer68-I.二叉搜索树的最近公共祖先/剑指Offer68-I-二叉搜索树的最近公共祖先.cpp
+++ 剑指Offer68-I.二叉搜索树的最近公共祖先/剑指Offer68-I-二叉搜索树的最近公共祖先.cpp
@@ -12,14 +12,14 @@ public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(root == NULL) return NULL;
         
-        if(p == root || q == root) return root;
-
-        TreeNode* left = lowestCommonAncestor(root->left,p,q);
-        TreeNode* right = lowestCommonAncestor(root->right,p,q);
-
-        if(left && right) return root;
-        else if(left) return left;
-        else if(right) return right;
-        else return NULL;
+        if(p->val<root->val && q->val<root->val){
+            return lowestCommonAncestor(root->left,p,q);
+        } 
+        else if(p->val>root->val && q->val>root->val){
+            return lowestCommonAncestor(root->right,p,q);
+        } 
+        else{
+            return root;
+        }
     }
 };
