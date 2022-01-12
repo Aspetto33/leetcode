b96b753a57aa3639d7e102abdcd4d293413a7ba4
‘Aspetto33 <1760906445@qq.com>
2022-01-12 11:08:20 +0800

    搜索与回溯

--- 剑指Offer38.字符串的排列/剑指Offer38-字符串的排列.cpp
+++ 剑指Offer38.字符串的排列/剑指Offer38-字符串的排列.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
-    bool checkInclusion(string s1, string s2) {
-        if(s2.size()<s1.size()) return false;
+    vector<string> permutation(string s) {
+        vector<string> result;
+        dfs(result,s,0);
+        return result;
+    }
 
-        vector<int> cnt1(26,0); //记录s1每个字符有多少
-        vector<int> cnt2(26,0); //记录s2每个字符有多少
-
-        //因为排列字符数量不变，所以将大小设为s1的大小
-        for(int i = 0;i<s1.size();i++){
-            cnt1[s1[i]-'a']++;
-            cnt2[s2[i]-'a']++;
+    void dfs(vector<string> &res,string &s,int pos){
+        if(pos == s.size()){
+            res.push_back(s);
         }
 
-        //如果两个数组相等，则返回true
-        if(cnt1 == cnt2){
-            return true;
-        }
+        for(int i = pos;i<s.size();i++){
+            bool flag = true;
 
-        //如果不相等，则移动滑动窗口，将后面的元素加入，将第一个元素移除
-        for(int i = s1.size();i<s2.size();i++){
-            cnt2[s2[i]-'a']++;
-            cnt2[s2[i-s1.size()]-'a']--;
-
-            if(cnt1 == cnt2){
-                return true;
+            for(int j = pos;j<i;j++){
+                if(s[j] == s[i]) flag = false;
             }
+            if(flag){
+                    swap(s[pos],s[i]);
+                    dfs(res,s,pos+1);
+                    swap(s[pos],s[i]);  
+                }
         }
-
-        return false;
     }
 };
