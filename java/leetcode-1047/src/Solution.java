55cd734d9438a335eaca814110066315b73b0eba
Aspetto343 <1760906445@qq.com>
2022-02-12 21:59:37 +0800

    增加用栈实现去除重复字符

---
+++ java/leetcode-1047/src/Solution.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+public class Solution {
+    public String removeDuplicates(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0;i < s.length();i++){
+
+            if(!stack.empty() && s.charAt(i) == stack.peek()){
+                stack.pop();
+            }
+            else{
+                stack.push(s.charAt(i));
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        while(!stack.empty()){
+            result.append(stack.pop());
+        }
+
+        return result.reverse().toString();
+    }
+}
