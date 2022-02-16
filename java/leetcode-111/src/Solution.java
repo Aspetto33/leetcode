eab425076228a1dcd887669c95fb3ae48305caa2
Aspetto343 <1760906445@qq.com>
2022-02-16 22:01:28 +0800

    增加求二叉树最小深度

---
+++ java/leetcode-111/src/Solution.java
@@ -0,0 +1,32 @@
+public class Solution {
+    public int minDepth(TreeNode root) {
+
+    }
+
+    class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+        public int minDepth(TreeNode root) {
+
+            if(root == null)
+                return 0;
+
+            if(root.left == null && root.right != null)
+                return 1 + minDepth(root.right);
+
+            if(root.left != null && root.right == null)
+                return 1 + minDepth(root.left);
+
+            return Math.min(minDepth(root.left),minDepth(root.right)) + 1;
+
+        }
+    }
+}
