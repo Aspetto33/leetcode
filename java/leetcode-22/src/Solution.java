b8f31370c38127f27de0cf6122afc93919475216
Aspetto343 <1760906445@qq.com>
2022-03-04 22:33:11 +0800

    增加求括号匹配的个数

---
+++ java/leetcode-22/src/Solution.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<String> generateParenthesis(int n) {
+
+        List<String> list = new ArrayList<>();
+        backtrack(list,new StringBuilder(),0,0,n);
+        return list;
+    }
+
+    public void backtrack(List<String> res,StringBuilder cur,int open,int close,int max){
+
+        if(cur.length() == max*2){
+            res.add(cur.toString());
+            return;
+        }
+
+        if(open < max){
+            cur.append('(');
+            backtrack(res,cur,open + 1,close,max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+
+        if(close < open){
+            cur.append(')');
+            backtrack(res, cur, open, close + 1, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+    }
+}
