26341f5152f5de9a189a8ccaef50393764ebd836
Aspetto343 <1760906445@qq.com>
2022-03-05 22:44:27 +0800

    增加dp求最大连续递增子序列

---
+++ java/leetcode-674/src/Solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int findLengthOfLCIS(int[] nums) {
+
+        int result = 1;
+        int[] dp = new int[nums.length];
+
+        for(int i = 0;i < nums.length;i++){
+            dp[i] = 1;
+        }
+
+        for(int i = 1;i < nums.length;i++){
+            if(dp[i] > dp[i-1])
+                dp[i] = dp[i-1] + 1;
+
+            result = Math.max(result,dp[i]);
+        }
+        return result;
+    }
+}
