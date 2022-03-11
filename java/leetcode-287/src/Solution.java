8411b11a3c8273fcd1e1025af949702e90b059ac
Aspetto343 <1760906445@qq.com>
2022-03-11 22:35:04 +0800

    增加使用map求重复数字

---
+++ java/leetcode-287/src/Solution.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public int findDuplicate(int[] nums) {
+
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int i = 0;i < nums.length;i++){
+            if(!map.containsKey(nums[i])){
+                map.put(nums[i],1);
+            }
+            else{
+                map.put(nums[i], map.get(nums[i])+1);
+
+                if(map.get(nums[i]) > 1){
+                    return nums[i];
+                }
+            }
+        }
+        return 0;
+    }
+}
